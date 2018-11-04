a8c04e167fcdae9c760401a54de2ec7b6f80e41e
rgriege <rgriege@sbcglobal.net>
2018-11-04 01:43:33 -0500

    Add rotating player

--- aster.c
+++ aster.c
@@ -4,12 +4,20 @@
 #define W 600
 #define H 600
 #define MAX_ASTEROIDS 16
+#define PLAYER_SIZE 20.f
+#define PLAYER_ROT_SPEED fPI
 
 struct asteroid
 {
 	v2f pos;
 	v2f vel;
 	s32 r;
+};
+
+struct player
+{
+	v2f pos;
+	v2f dir;
 };
 
 static
@@ -42,12 +50,25 @@ void generate_asteroid(struct asteroid *a)
 	}
 }
 
+static
+void render_player(gui_t *gui, const struct player *player)
+{
+	const v2f lperp = v2f_lperp(player->dir);
+	const v2f tri[3] = {
+		v2f_fmadd(player->pos, lperp, PLAYER_SIZE/2),
+		v2f_fmadd(player->pos, lperp, -PLAYER_SIZE/2),
+		v2f_fmadd(player->pos, player->dir, PLAYER_SIZE/2),
+	};
+	gui_polyf(gui, B2PC(tri), g_red, g_white);
+}
+
 int main(int argc, char *const argv[])
 {
 	gui_t *gui;
 	b32 quit = false;
 	struct asteroid asteroids[MAX_ASTEROIDS] = {0};
 	u32 asteroid_timer = 0;
+	struct player player = { .pos = { .x=W/2, .y=H/2 }, .dir = g_v2f_up };
 
 	gui = gui_create(0, 0, W, H, "aster", WINDOW_CENTERED);
 	if (!gui)
@@ -86,6 +107,12 @@ int main(int argc, char *const argv[])
 					a->r = 0;
 			}
 		}
+
+		if (key_down(gui, KB_A))
+			player.dir = v2f_rot(player.dir, PLAYER_ROT_SPEED*dt);
+		else if (key_down(gui, KB_D))
+			player.dir = v2f_rot(player.dir, -PLAYER_ROT_SPEED*dt);
+		render_player(gui, &player);
 
 		if (key_down(gui, KB_Q))
 			quit = true;
